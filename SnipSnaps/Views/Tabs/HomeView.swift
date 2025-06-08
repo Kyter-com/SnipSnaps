c96a9726a1ff03d264a677941b46b4c7d70ea07a
Nick Reisenauer <nickdreisenauer@gmail.com>
2025-06-07 22:19:46 -0700

    handle cases

--- SnipSnaps/Views/Tabs/HomeView.swift
+++ SnipSnaps/Views/Tabs/HomeView.swift
@@ -9,14 +9,31 @@ import Photos
 import SwiftUI
 
 struct HomeView: View {
+  @State private var photoAuthStatus: Bool = false
+
   var body: some View {
-    Text("Home")
-    //    Button("Request Photo Access") {
-    //      PHPhotoLibrary.requestAuthorization(for: .readWrite) { status in
-    //        DispatchQueue.main.async {
-    //          let status = getPhotoAuthStatus()
-    //        }
-    //      }
-    //    }
+    VStack {
+      if !photoAuthStatus {
+        Button("Request Photo Access") {
+          PHPhotoLibrary.requestAuthorization(for: .readWrite) { status in
+            DispatchQueue.main.async {
+              photoAuthStatus = getPhotoAuthStatus()
+            }
+          }
+        }
+      }
+      Text(photoAuthStatus ? "Photo access granted" : "Photo access denied")
+      Button("Screenshots") {
+        // Handle screenshots action
+      }
+      Button("Random") {
+        // Handle random action
+      }
+    }
+    .onAppear {
+      Task {
+        photoAuthStatus = getPhotoAuthStatus()
+      }
+    }
   }
 }
